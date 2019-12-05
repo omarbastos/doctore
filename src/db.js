ff7dfdbc09edb90049f24f6b481d1611bd24c1cd
Antonio Hernandez <antoniohernandez@gmail.com>
2019-12-05 11:50:17 -0400

    Registro por post y encriptado de password probado

--- src/db.js
+++ src/db.js
@@ -1,14 +1,17 @@
-const db = require("mongoose");
-const MONGO_URI = `mongodb+srv://doctoreadmin:!$qjbQt8DfEAsPQ@cluster0-0p7bv.mongodb.net/test?retryWrites=true&w=majority`;
+const db = require('mongoose')
+const colors = require('colors')
+const MONGO_URI = `mongodb+srv://doctoreadmin:!$qjbQt8DfEAsPQ@cluster0-0p7bv.mongodb.net/test?retryWrites=true&w=majority`
 
-db.Promise = global.Promise;
+db.Promise = global.Promise
+
 async function connect() {
   await db.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  });
+  })
 
-  console.log("[db] Conectada con exito");
+  console.log("[db] conectada exitosamente.".green)
+  console.log('Ejecuta npn run serve para iniciar el Frontend.'.blue)
 }
 
-module.exports = connect;
+module.exports = connect
