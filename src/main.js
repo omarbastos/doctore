f2f157b26dd71aed7056e6196cf667d8ecc351df
Omar Hernandez <omarsro@gmail.com>
2019-12-11 09:25:41 -0400

    Archivos limpios para firebase

--- src/main.js
+++ src/main.js
@@ -26,21 +26,11 @@ Vue.use(VueApexCharts);
 Vue.component("apexchart", VueApexCharts);
 Vue.component("marquee-text", MarqueeText);
 Vue.config.productionTip = false;
-import axios from "axios";
-import VueAxios from "vue-axios";
-Vue.prototype.$http = axios;
-const token = localStorage.getItem("token");
-if (token) {
-  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
-}
-Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
-
   vuetify,
-
   created() {
     AOS.init();
   },
--- src/store/index.js
+++ src/store/index.js
@@ -1,103 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     status: "",
-    token: localStorage.getItem("token") || "",
+    token: "Token",
     user: {
-      username: localStorage.getItem("username") || "",
-      level: localStorage.getItem("level") || "",
-      grupo: localStorage.getItem("grupo") || ""
+      username: "omarbastos", //localStorage.getItem("username") || "",
+      level: "Agente", //localStorage.getItem("level") || "",
+      grupo: "Claro" //localStorage.getItem("grupo") || ""
     }
   },
-  mutations: {
-    auth_request(state) {
-      state.status = "loading";
-    },
-    auth_success(state, { token, user }) {
-      state.status = "success";
-      state.token = token;
-      state.user.username = user.username;
-      state.user.level = user.level;
-      state.user.grupo = user.grupo;
-    },
-    auth_eror(state) {
-      state.status = "error";
-    },
-    logout(state) {
-      state.status = "";
-      state.token = "";
-      state.user.username = "";
-      state.user.level = "";
-      state.user.grupo = "";
-    }
-  },
-  actions: {
-    logout({ commit }) {
-      return new Promise(resolve => {
-        commit("logout");
-        localStorage.removeItem("token");
-
-        localStorage.removeItem("username");
-
-        localStorage.removeItem("level");
-
-        localStorage.removeItem("grupo");
-        delete axios.defaults.headers.common["Authorization"];
-        resolve();
-      });
-    },
-    register({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit("auth_request");
-        axios({
-          url: "http://localhost:3000/api/auth/register",
-          data: user,
-          method: "POST"
-        })
-          .then(resp => {
-            resolve(resp);
-          })
-          .catch(err => {
-            commit("auth_error", err);
-            localStorage.removeItem("token");
-            reject(err);
-          });
-      });
-    },
-    login({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit("auth_request");
-        axios({
-          url: "http://localhost:3000/api/auth/login",
-          data: user,
-          method: "POST"
-        })
-          .then(resp => {
-            const token = resp.data.token;
-            const user = resp.data.user;
-
-            localStorage.setItem("token", token);
-            //guardando en local storage
-            localStorage.setItem("username", user.username);
-            localStorage.setItem("level", user.level);
-            localStorage.setItem("grupo", user.grupo);
-            axios.defaults.headers.common["Authorization"] = token;
-            commit("auth_success", { token, user });
-            resolve(resp);
-          })
-          .catch(err => {
-            commit("auth_error");
-            console.log(err);
-            localStorage.removeItem("token");
-            reject(err);
-          });
-      });
-    }
-  },
+  mutations: {},
+  actions: {},
   getters: {
     isLoggedIn: state => !!state.token,
     authStatus: state => state.status,
